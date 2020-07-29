b495a3081d9bccb76de6862c66ef12412aff844e
WendahNyiawung <42493128+WendahNyiawung@users.noreply.github.com>
2020-07-29 18:21:46 +0100

    done with navbar for desktop

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+function openTab(cityName) {
+    var i;
+    var x = document.getElementsByClassName("tab");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    document.getElementById(cityName).style.display = "block";  
+  }
