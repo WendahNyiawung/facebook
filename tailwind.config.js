5734887123b0d1e4f8249c87bc77d7a8cc6531d9
WendahNyiawung <42493128+WendahNyiawung@users.noreply.github.com>
2020-07-29 17:06:29 +0100

    done with left and right nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   purge: [],
   theme: {
+    screens: {
+      'sm': {'min': '320px', 'max': '767px'},
+      'md': {'min': '768px', 'max': '1024px'},
+      'lg': {'min': '1024px', 'max': '1279px'},
+      'xl': {'min': '1280px'},
+    },
     extend: {},
   },
   variants: {},
